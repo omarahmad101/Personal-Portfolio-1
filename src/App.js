f7df6836820e2bbac183ef23f56a6e23202f7708
Omar Ahmad Dahy <82223672+omarahmad101@users.noreply.github.com>
2024-07-06 07:44:46 +0300

    start new project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar/navbar"; // Change! Capitalized Navbar
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <Navbar /> {/* Change! Capitalized Navbar */}
+    </div>
+  );
+}
+
+export default App;
